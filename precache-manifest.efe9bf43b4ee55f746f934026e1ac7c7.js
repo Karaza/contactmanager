b0994d248472a404035b1152203db7e077f72b34
Sophie Kowalski <skowalski57@gmail.com>
2018-11-22 11:14:16 +0100

    Updates

---
+++ precache-manifest.efe9bf43b4ee55f746f934026e1ac7c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "7187e8d6cb2bd0f3d560",
+    "url": "/contactmanager/static/js/main.7187e8d6.chunk.js"
+  },
+  {
+    "revision": "79d8c0fd250ba6ca1c0b",
+    "url": "/contactmanager/static/js/1.79d8c0fd.chunk.js"
+  },
+  {
+    "revision": "7187e8d6cb2bd0f3d560",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "79d8c0fd250ba6ca1c0b",
+    "url": "/contactmanager/static/css/1.4ee8ed52.chunk.css"
+  },
+  {
+    "revision": "5606dc82b8cce9084b8c327a48d4848d",
+    "url": "/contactmanager/index.html"
+  }
+];
